646cd50e36193274738cac557f5ec77d51c18ed4
fffnite <bradley.whiteford@live.co.uk>
2019-05-02 00:53:47 +0100

    Add working sqlite database support

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+    "fmt"
     "net/http"
     "database/sql"
     _ "github.com/mattn/go-sqlite3"
     "github.com/go-chi/chi"
     "GoOneRoster/routes"
+)
+
+// To be moved to conf.hcl
+const (
+    dbDSN = "OneRoster.s3db"
+    dbDriver = "sqlite3"
+)
+
+var (
+    db  *sql.DB
+    err error
 )
 
 // generic catch function for error handling
@@ -15,8 +27,30 @@ func catch(err error) {
     }
 }
 
+// Create basic database 
+func dbMake(db *sql.DB) {
+    _, err = db.Exec("CREATE TABLE IF NOT EXISTS test ( name string, age int )")
+    catch (err)
+    _, err = db.Exec("INSERT INTO test (name, age) VALUES ('bob', 1)")
+    catch (err)
+}
+
+// Query database
+func dbOut(db *sql.DB) {
+    var name string
+    err := db.QueryRow("SELECT name FROM test").Scan(&name)
+    catch(err)
+    fmt.Print(name)
+}
+
 func main() {
     r := chi.NewRouter()
+
+    db, err = sql.Open(dbDriver, dbDSN)
+    catch (err)
+    defer db.Close()
+    dbMake(db)
+    dbOut(db)
 
     // Creates a root endpoint with get method returning helloWorld func results
     r.Get("/", helloWorld)
