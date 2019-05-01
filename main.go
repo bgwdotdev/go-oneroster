41f7d28aaa1987f8937b1afd3a65a91582eef25b
bw652 <b.whiteford@tga.org.uk>
2019-05-01 18:02:39 +0100

    Add endpoint and base route for users

--- main.go
+++ main.go
@@ -7,10 +7,19 @@ import (
 
 func main() {
     r := chi.NewRouter()
+
     r.Get("/", helloWorld)
+    r.Route("/users", func (r chi.Router) {
+        r.Get("/", allUsers)
+    })
+
     http.ListenAndServe(":3000", r)
 }
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
     w.Write([]byte("Hello World!"))
+}
+
+func allUsers(w http.ResponseWriter, r *http.Request) {
+    w.Write([]byte("user: bob"))
 }
