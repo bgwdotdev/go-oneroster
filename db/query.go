87feb70dae709e7af1dfdfe261a409c3167dd18d
fffnite <bradley.whiteford@live.co.uk>
2019-05-18 19:27:15 +0100

    Add db dependency

--- db/query.go
+++ db/query.go
@@ -6,7 +6,8 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-func QueryProperties(t string, c []string, q []map[string]string) *sql.Rows {
+// Queries the database based off endpoint
+func QueryProperties(t string, c []string, q []map[string]string, db *sql.DB) *sql.Rows {
 	s := fmt.Sprintf("SELECT %v FROM %v WHERE %v%v? %v %v%v? ORDER BY ? LIMIT ? OFFSET ?",
 		q["columns"], t,
 		q["filter"][0]["field"], q["filter"][0]["predicate"],
