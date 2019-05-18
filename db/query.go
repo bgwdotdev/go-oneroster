475875c39efbbb37fff859dff440a5ed3ec1f4e7
fffnite <bradley.whiteford@live.co.uk>
2019-05-18 22:44:06 +0100

    Refactor sql query using squirrel

--- db/query.go
+++ db/query.go
@@ -1,19 +1,24 @@
 package db
 
 import (
+	"GoOneRoster/parameters"
 	"database/sql"
 	"fmt"
+	"github.com/Masterminds/squirrel"
 	_ "github.com/mattn/go-sqlite3"
 )
 
 // Queries the database based off endpoint
-func QueryProperties(t string, c []string, q []map[string]string, db *sql.DB) *sql.Rows {
-	s := fmt.Sprintf("SELECT %v FROM %v WHERE %v%v? %v %v%v? ORDER BY ? LIMIT ? OFFSET ?",
-		q["columns"], t,
-		q["filter"][0]["field"], q["filter"][0]["predicate"],
-		q["filter"]["logicalOperator"],
-		q["filter"][1]["field"], q["filter"][1]["predicate"],
-	)
+func QueryProperties(t string, c []string, p *parameters.Parameters, db *sql.DB) *sql.Rows {
+	w := fmt.Sprintf("%v%v? %v %v%v?", p.F1.field, p.F1.Predicate, p.LogicalOperator, p.F2.field, p.F2.Predicate)
+	s := squirrel.
+		Select(p.Fields).
+		From(t).
+		Where(w, p.F1.value, p.F1.value).
+		OrderBy(p.Sort).
+		Limit(p.Limit).
+		Offset(p.Offset).
+		ToSql()
 
 	stmt, err := db.Prepare(s)
 	if err != nil {
@@ -21,8 +26,7 @@ func QueryProperties(t string, c []string, q []map[string]string, db *sql.DB) *s
 	}
 	defer stmt.Close()
 
-	rows, err := stmt.Query(q["filter"][0]["value"], q["filter"][1]["value"],
-		q["sort"], q["limit"], q["offset"])
+	rows, err := stmt.Query()
 	if err != nil {
 		panic(err)
 	}
