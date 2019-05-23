79904109e4eb4cd630ac25ee2d021d42ff804c7c
fffnite <bradley.whiteford@live.co.uk>
2019-05-23 01:31:23 +0100

    Add error handling function

---
+++ handlers/err.go
@@ -0,0 +1,57 @@
+package handlers
+
+import (
+	"github.com/go-chi/render"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type Error struct {
+	StatusCode  int    `json:"-"`
+	CodeMajor   string `json:"codeMajor"`
+	Severity    string `json:"severity"`
+	Description string `json:"description"`
+	CodeMinor   string `json:"codeMinor"`
+}
+
+func (e Error) Payload(w http.ResponseWriter, r *http.Request) {
+	e = validateCodeMinor(e)
+	log.Info(e)
+	render.Status(r, e.StatusCode)
+	render.JSON(w, r, e)
+}
+
+func validateCodeMinor(e Error) Error {
+	c := e.CodeMinor
+	switch c {
+	case "full success":
+		e.StatusCode = http.StatusOK
+		e.CodeMajor = "success"
+		e.Severity = "status"
+	case "invalid_sort_field", "invalid_selection_field":
+		e.StatusCode = http.StatusOK
+		e.CodeMajor = "success"
+		e.Severity = "warning"
+	case "invalid data", "invalid_filter_field", "invalid_blank_selection_field":
+		e.StatusCode = http.StatusBadRequest
+		e.CodeMajor = "failure"
+		e.Severity = "error"
+	case "unauthorized":
+		e.StatusCode = http.StatusUnauthorized
+		e.CodeMajor = "failure"
+		e.Severity = "error"
+	case "forbidden":
+		e.StatusCode = http.StatusForbidden
+		e.CodeMajor = "failure"
+		e.Severity = "error"
+	case "unknown object":
+		e.StatusCode = http.StatusNotFound
+		e.CodeMajor = "failure"
+		e.Severity = "error"
+	case "server_busy":
+		e.StatusCode = http.StatusTooManyRequests
+		e.CodeMajor = "failure"
+		e.Severity = "error"
+	}
+	return e
+}
