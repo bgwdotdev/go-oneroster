85a915119d5697fb4a66369055b0adfeabeff574
fffnite <bradley.whiteford@live.co.uk>
2019-08-04 01:57:16 +0100

    Add oauth2 implementation

--- conf/conf.go
+++ conf/conf.go
@@ -2,6 +2,8 @@ package conf
 
 import (
 	"github.com/spf13/viper"
+	errors "golang.org/x/xerrors"
+	"os"
 )
 
 // Holds the config loaded from file
@@ -11,6 +13,25 @@ type Config struct {
 		DatabaseDriver string
 		DataSourceName string
 	}
+}
+
+type AuthConfig struct {
+	Key    string
+	KeyAlg string
+}
+
+// inits the JWT authorization config from environment variables
+func (c *AuthConfig) Load() error {
+	var ok bool
+	c.Key, ok = os.LookupEnv("GOR_AUTH_KEY")
+	if !ok {
+		return errors.New("No JWT secret, set env GOR_AUTH_KEY")
+	}
+	c.KeyAlg, ok = os.LookupEnv("GOR_AUTH_KEYALG")
+	if !ok {
+		return errors.New("No JWT Key algrithm, set env GOR_AUTH_KEYALG; default HS256")
+	}
+	return nil
 }
 
 // Invokes Viper to read the configuration file
