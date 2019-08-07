91efd67450e96f3dfe663216e0fbd787e8c6d0bb
fffnite <bradley.whiteford@live.co.uk>
2019-08-07 23:42:12 +0100

    Add more base tables

--- build/db/orgs.sql
+++ build/db/orgs.sql
@@ -1,10 +1,42 @@
-CREATE TABLE IF NOT EXISTS orgs (
-    Id Integer PRIMARY KEY AUTOINCREMENT,
-    sourcedId text,
-    status text,
-    dateLastModified text,
-    name text,
-    type text,
-    identifier text,
-    parentSourcedId text
+CREATE TABLE IF NOT EXISTS "orgs" (
+    "Id" Integer PRIMARY KEY AUTOINCREMENT,
+    "sourcedId" text,
+    "status" text,
+    "dateLastModified" text,
+    "name" text,
+    "type" text,
+    "identifier" text,
+    "parentSourcedId" text,
+    CONTRAINT "FK_orgs_orgs_parentSourcedId" FOREIGN KEY ("parentSourcedId") REFERENCES "orgs" ("sourcedId")
 );
+
+CREATE TABLE IF NOT EXISTS "academicSessions" (
+    "Id" Integer PRIMARY KEY AUTOINCREMENT,
+    "sourcedId" text,
+    "status" text,
+    "dateLastModified" text,
+    "title" text,
+    "type" text,
+    "startDate" text,
+    "endDate" text,
+    "parentSourcedId" text,
+    "schoolYear" text,
+    CONSTRAINT "FK_academicSessions_academicSessions_parentSourcedId" FOREIGN KEY ("parentSourcedId") REFERENCES "academicSessions" ("sourcedId")
+);
+
+CREATE TABLE IF NOT EXISTS "courses" (
+    "Id" Integer PRIMARY KEY AUTOINCREMENT,
+    "sourcedId" text,
+    "status" text,
+    "dateLastModified" text,
+    "schoolYearSourcedId" text,
+    "title" text,
+    "courseCode" text,
+    "grades" text,
+    "orgSourcedId" text,
+    "subjects" text,
+    "subjectCodes" text,
+    CONSTRAINT "FK_courses_academicSessions_schoolYearSourcedId" FOREIGN KEY ("schoolYearSourcedId") REFERENCES "academicSessions" ("sourcedId")
+);
+
+CREATE TABLE IF NOT EXISTS "classes" (
