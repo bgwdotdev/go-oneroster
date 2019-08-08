abca10c323605b9ae30f6c3885f348925b52880e
fffnite <bradley.whiteford@live.co.uk>
2019-08-08 23:14:15 +0100

    Add sample data generation file

---
+++ demo/sampledata.sql
@@ -0,0 +1,33 @@
+INSERT INTO "academicSessions" (
+    "sourcedId",
+    "status",
+    "dateLastModified",
+    "title",
+    "type",
+    "startDate",
+    "endDate",
+    "parentSourcedId",
+    "schoolYear"
+)
+VALUES (
+    "01",
+    "active",
+    "2019-08-08T22:44:00:000Z",
+    "session 2019/20",
+    "schoolYear",
+    "2019-08-26",
+    "2020-06-30",
+    "2a283310-7778-438f-914d-0e5de3afd7ed",
+    "2019/20"
+),
+(
+    "00",
+    "active",
+    "2018-08-08T09:00:00:000Z",
+    "session 2018/19",
+    "schoolYear",
+    "2018-08-26",
+    "2019-06-30",
+    "2a283310-7778-438f-914d-0e5de3afd7ed",
+    "2018/2019"
+);
