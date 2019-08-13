4b7d4d773d3af47a96a698cd46225978efb31235
bw652 <b.whiteford@tga.org.uk>
2019-08-13 17:31:02 +0100

    Add filter, homeroom and non-pupil user queries

--- build/sync-model/wcbs-pass.sql
+++ build/sync-model/wcbs-pass.sql
@@ -4,10 +4,12 @@ SELECT
     CODE as name,
     'school' AS type,
     DESCRIPTION AS identifier,
-    LAST_AMEND_DATE as dateLastModified,
+    LAST_AMEND_DATE AS dateLastModified,
     IN_USE AS status,
 FROM 
     dbo.SCHOOL
+ORDER BY
+    SCHOOL_ID
 
 /** academicSession - academic Years **/
 SELECT
@@ -20,8 +22,9 @@ SELECT
     Y.CODE AS schoolYear
 FROM 
     dbo.YEAR AS Y INNER JOIN
-    dbo.SCHOOL_CALENDAR AS SC on SC.ACADEMIC_YEAR = Y.CODE
-
+    dbo.SCHOOL_CALENDAR AS SC ON SC.ACADEMIC_YEAR = Y.CODE
+ORDER BY 
+    Y.YEAR_ID
 /** academicSession - terms **/
 
 /** courses **/
@@ -41,6 +44,8 @@ FROM
         INNER JOIN
     dbo.SCHOOL AS org
         ON org.CODE = S.SCHOOL
+ORDER BY
+    S.SUBJECT_ID
 
 /** classes - scheduled **/ 
 SELECT
@@ -48,16 +53,16 @@ SELECT
     S.IN_USE AS status,
     S.LAST_AMEND_DATE AS dateLastModified,
     S.DESCRIPTION AS title,
-    /* grades - N/A */
-    /* course soucedid */
+    /* null AS grades, */
+    SUB.SUBJECT_ID AS courseSoucedId,
     S.SET_CODE AS classCode,
     'scheduled' AS classType,
     S.ROOM AS location,
     org.SCHOOL_ID AS schoolSourcedId,
     S.SUBJECT_SET_ID AS termSourcedIds, 
     SUB.DESCRIPTION AS subjects
-    /* subjectCodes - SQA codes? */
-    /* periods - N/A? */ 
+    /* SQA CODES? AS subjectCodes, */
+    /* null AS periods */ 
 FROM
     dbo.SUBJECT_SET AS S
         INNER JOIN
@@ -69,8 +74,33 @@ FROM
         INNER JOIN
     dbo.SUBJECT AS SUB
         ON SUB.CODE = S.SUBJECT
-
+ORDER BY
+    S.SUBJECT_SET_ID    
 /** classes - homeroom(form) **/
+SELECT
+    F.FORM_ID AS sourcedId,
+    F.IN_USE AS status,
+    F.LAST_AMEND_DATE AS dateLastModified,
+    F.DESCRIPTION AS title,
+    /* null AS grades, */
+    /* null AS courseSourcedId */
+    F.CODE AS classCode,
+    'homeroom' AS classType,
+    F.ROOM AS location,
+    org.SCHOOL_ID AS schoolSourcedId,
+    F.FORM_ID AS termSourcedIds
+    /* null AS subjects */
+    /* null AS subjectCodes */
+    /* null as periods */
+FROM
+    dbo.FORM AS F
+        INNER JOIN
+    dbo.SCHOOL AS org
+        ON org.CODE = F.SCHOOL
+WHERE
+    F.ACADEMIC_YEAR = '2019'
+ORDER BY
+    F.FORM_ID
 
 /** users - pupils **/
 SELECT
@@ -95,9 +125,6 @@ SELECT
 FROM
     dbo.PUPIL AS P
         INNER JOIN
-    dbo.SCHOOL as org
-        ON org.CODE = P.SCHOOL
-        INNER JOIN
     dbo.NAME AS N
         ON P.NAME_ID = N.NAME_ID
         INNER JOIN
@@ -106,4 +133,71 @@ FROM
         INNER JOIN
     dbo.FORM_YEAR AS formYear
         ON form.YEAR_CODE = formYear.CODE
-
+WHERE 
+    P.ACADEMIC_YEAR = '2019'
+    AND
+    form.ACADEMC_YEAR = '2019'
+ORDER BY
+    P.NAME_ID
+/** users - staff **/
+SELECT 
+    U.NAME_ID AS sourcedId,
+    U.IN_USE AS status,
+    U.LAST_AMEND_DATE AS dateLastModified,
+    U.NAME_ID AS orgSourcedIds,
+    'teacher' AS role,
+    U.INTERNAL_EMAIL_ADDRESS AS username,
+    U.NAME_ID as userIds,
+    N.PREFERRED_NAME AS givenName,
+    N.SURNAME AS familyname,
+    /* null AS middlename, */
+    U.CODE AS identifier,
+    U.INTERNAL_EMAIL_ADDRESS AS email,
+    /* null AS sms, */
+    /* null AS phone, */
+    U.NAME_ID AS agentSourcedIds
+    /* null AS grades, */
+    /* null AS password */
+FROM
+    dbo.STAFF as U
+        INNER JOIN
+    dbo.NAME AS N
+        ON N.NAME_ID = U.NAME_ID
+WHERE
+    U.CATEGORY = 'TEA001'
+    OR
+    U.CATEGORY = 'SUPPLY'
+    OR
+    U.CATEGORY = 'EARLY'
+ORDER BY
+    U.NAME_ID
+/** users - support staff **/
+SELECT 
+    U.NAME_ID AS sourcedId,
+    U.IN_USE AS status,
+    U.LAST_AMEND_DATE AS dateLastModified,
+    U.NAME_ID AS orgSourcedIds,
+    'aide' AS role,
+    U.INTERNAL_EMAIL_ADDRESS AS username,
+    U.NAME_ID as userIds,
+    N.PREFERRED_NAME AS givenName,
+    N.SURNAME AS familyname,
+    /* null AS middlename, */
+    U.CODE AS identifier,
+    U.INTERNAL_EMAIL_ADDRESS AS email,
+    /* null AS sms, */
+    /* null AS phone, */
+    U.NAME_ID AS agentSourcedIds
+    /* null AS grades, */
+    /* null AS password */
+FROM
+    dbo.STAFF as U
+        INNER JOIN
+    dbo.NAME AS N
+        ON N.NAME_ID = U.NAME_ID
+WHERE
+    U.CATEGORY = 'NON001'
+    OR
+    U.CATEGORY = 'COACH'
+ORDER BY
+    U.NAME_ID
