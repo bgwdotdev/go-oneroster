8c79e7586d5d7dd863f9db42b8a95c35ed7c20b3
fffnite <bradley.whiteford@live.co.uk>
2019-09-22 15:58:00 +0100

    Add sort, filter and error handling

--- helpers/parameters.go
+++ helpers/parameters.go
@@ -2,17 +2,44 @@ package helpers
 
 import ()
 
-func SetOptions() (*mongo.FindOptions, error) {
+func GetOptions() (*mongo.FindOptions, []error) {
+	var errP []error
 	projection, err := getFields(url)
-	options.
+	if err != nil {
+		errP = append(errP, err)
+	}
+	sort, err := getSort(url)
+	if err != nil {
+		errP = append(errP, err)
+	}
+	o := options.
 		Find().
 		SetLimit(getLimit(url)).
 		SetSkip(getOffset(url)).
-		SetSort(bson.D{getSort(url), 1}).
-		SetProjection(projection)
+		SetSort(bson.D{sort, 1}).
+		SetProjection(bson.D{projection})
+	return o, ep
 }
 
-func getFilters() {}
+func GetFilters(q url.Values, safeFields []string) (bson.D, error) {
+	v := q.Get("filter")
+	if v != "" {
+		lo := parseFilterLo(v)
+		var filter []bson.DocElem
+		queries := splitFilterQuery(v)
+		for _, f := range queries {
+			ff, err := parseFilterField(f, safeFields)
+			if err != nil {
+				return bson.D{}, err
+			}
+			fp := parseFilterPredicate(f)
+			fv := parseFilterValue(f)
+			filter = append(filter, bson.DocElem{ff, bson.D{fp, fv}})
+		}
+		return bson.D{{lo, filter}}, nil
+	}
+	return bson.D{}, nil
+}
 
 func getFields(q url.Values, safeFields []string) ([]bson.DocElem, error) {
 	v := q.Get("fields")
@@ -33,6 +60,21 @@ func getFields(q url.Values, safeFields []string) ([]bson.DocElem, error) {
 	return fields, nil
 }
 
+func getSort(q url.Values, safeFields []string) (string, error) {
+	v := q.Get("sort")
+	d := "sourcedId"
+	if v != "" {
+		f, err := validateField(v, safeCol)
+		if err != nil {
+			err.(*ErrorObject).CodeMinor = "invalid_sort_field"
+			err.(*ErrorObject).Populate()
+			return d, err
+		}
+		return f, nil
+	}
+	return d
+}
+
 func getLimit(q url.Values) int {
 	v := q.Get("limit")
 	if v != "" {
@@ -47,12 +89,4 @@ func getOffset(q url.Values) int {
 		return v.ToInt()
 	}
 	return 0
-}
-
-func getSort(q url.Values) string {
-	v := q.Get("sort")
-	if v != "" {
-		return v
-	}
-	return "sourcedId"
 }
