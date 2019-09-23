abdfc8cff5f49cc8b3379fbdad76d8ec4d1abfd2
fffnite <bradley.whiteford@live.co.uk>
2019-09-23 17:49:32 +0100

    Refrac classes endpoints to mongodb

--- handlers/classes.go
+++ handlers/classes.go
@@ -1,31 +1,27 @@
 package handlers
 
 import (
-	"database/sql"
-	"github.com/fffnite/go-oneroster/parameters"
-	_ "github.com/mattn/go-sqlite3"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"time"
 )
 
-func GetAllClasses(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		api := apiRequest{
-			Request: Req{w, r},
-			DB:      db,
-			ORData: OneRoster{
-				Table:      "classes",
-				Columns:    classesCols,
-				OutputName: "classes",
-			},
-			Params: parameters.Parameters{},
-			Fks: []FK{
-				FK{"courseSourcedId", "courses", "sourcedId", "sourcedId", "course"},
-				FK{"termSourcedIds", "classAcademicSessions", "classSourcedId", "academicSessionSourcedId", "terms"},
-				FK{"schoolSourcedId", "orgs", "sourcedId", "sourcedId", "school"},
-			},
-		}
-		api.invoke()
-	}
+type classes struct {
+	SourcedId        string    `json:"sourcedId" bson:"sourcedId,omitempty"`
+	Status           string    `json:"status" bson:"status,omitempty"`
+	DateLastModified time.Time `json:"dateLastModified" bson:"dateLastModified,omitempty"`
+	Title            string    `json:"title" bson:"title,omitempty"`
+	ClassCode        string    `json:"classCode" bson:"classCode,omitempty"`
+	ClassType        string    `json:"classType" bson:"classType,omitempty"`
+	Location         string    `json:"location" bson:"location,omitempty"`
+	Grades           []string  `json:"grades" bson:"grades,omitempty"`
+	Subjects         []string  `json:"subjects" bson:"subjects,omitempty"`
+	Course           string    `json:"course" bson:"course,omitempty"`
+	School           string    `json:"school" bson:"school,omitempty"`
+	Terms            []*Nested `json:"terms" bson:"terms,omitempty"`
+	SubjectCodes     []string  `json:"subjectCodes" bson:"subjectCodes,omitempty"`
+	Periods          []string  `json:"periods" bson:"periods,omitempty"`
+	Resources        []*Nested `json:"resources" bson:"resources,omitempty"`
 }
 
 var classesCols = []string{
@@ -33,14 +29,38 @@ var classesCols = []string{
 	"status",
 	"dateLastModified",
 	"title",
-	"grades",
-	"courseSourcedId",
 	"classCode",
 	"classType",
 	"location",
-	"schoolSourcedId",
-	"termSourcedIds",
+	"grades",
 	"subjects",
+	"course",
+	"school",
+	"terms",
 	"subjectCodes",
 	"periods",
+	"resources",
+}
+
+func GetAllClasses(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("classes")
+		GetCollection(c, asCols, w, r)
+	}
+}
+
+func GetClasses(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("classes")
+		GetDoc(c, asCols, w, r)
+	}
+}
+
+func PutClasses(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("classes")
+		var data classes
+		data.DateLastModified = time.Now()
+		PutDoc(c, &data, w, r)
+	}
 }
