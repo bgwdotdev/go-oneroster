54bbd833a8142f02651e327260554c77f40fc057
fffnite <bradley.whiteford@live.co.uk>
2019-09-23 16:41:02 +0100

    Add and refrac academicsession funcs to mongodb

--- handlers/academicSessions.go
+++ handlers/academicSessions.go
@@ -1,27 +1,22 @@
 package handlers
 
 import (
-	"database/sql"
-	"github.com/fffnite/go-oneroster/parameters"
-	_ "github.com/mattn/go-sqlite3"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"time"
 )
 
-func GetAllAcademicSessions(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		api := apiRequest{
-			Request: Req{w, r},
-			DB:      db,
-			ORData: OneRoster{
-				Table:      "academicSessions",
-				Columns:    asCols,
-				OutputName: "academicSessions",
-			},
-			Params: parameters.Parameters{},
-			Fks:    []FK{FK{"parentSourcedId", "academicSessions", "sourcedId", "sourcedId", "parent"}},
-		}
-		api.invoke()
-	}
+type academicSessions struct {
+	SourcedId        string    `json:"sourcedId" bson:"sourcedId,omitempty"`
+	Status           string    `json:"status" bson:"status,omitempty"`
+	DateLastModified time.Time `json:"dateLastModified" bson:"dateLastModified,omitempty"`
+	Title            string    `json:"title" bson:"title,omitempty"`
+	StartDate        string    `json:"startDate" bson:"startDate,omitempty"`
+	EndDate          string    `json:"endDate" bson:"endDate,omitempty"`
+	Type             string    `json:"type" bson:"type,omitempty"`
+	Parent           *Nested   `json:"parent" bson:"parent,omitempty"`
+	Children         []*Nested `json:"children" bson:"children,omitempty"`
+	SchoolYear       string    `json:"schoolYear" bson:"schoolYear,omitempty"`
 }
 
 var asCols = []string{
@@ -29,9 +24,33 @@ var asCols = []string{
 	"status",
 	"dateLastModified",
 	"title",
-	"type",
 	"startDate",
 	"endDate",
-	"parentSourcedId",
+	"type",
+	"parent",
+	"children",
 	"schoolYear",
+}
+
+func GetAllAcademicSessions(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("academicSessions")
+		GetCollection(c, asCols, w, r)
+	}
+}
+
+func GetAcademicSession(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("academicSessions")
+		GetDoc(c, asCols, w, r)
+	}
+}
+
+func PutAcademicSession(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("academicSessions")
+		var data academicSessions
+		data.DateLastModified = time.Now()
+		PutDoc(c, &data, w, r)
+	}
 }
