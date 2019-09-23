16359991fe0393e8ba2c51e5faf36353b7296e2b
fffnite <bradley.whiteford@live.co.uk>
2019-09-23 18:36:21 +0100

    refrac users endpoints for mongodb

--- handlers/users.go
+++ handlers/users.go
@@ -1,49 +1,77 @@
 package handlers
 
 import (
-	"database/sql"
-	"github.com/fffnite/go-oneroster/parameters"
-	_ "github.com/mattn/go-sqlite3"
+	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
+	"time"
 )
 
-func GetAllUsers(db *sql.DB) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		api := apiRequest{
-			Request: Req{w, r},
-			DB:      db,
-			ORData: OneRoster{
-				Table:      "users",
-				Columns:    userCols,
-				OutputName: "users",
-			},
-			Params: parameters.Parameters{},
-			Fks: []FK{
-				FK{"agentSourcedIds", "userAgents", "userSourcedId", "agentSourcedId", "agents"},
-				FK{"orgSourcedIds", "userOrgs", "userSourcedId", "orgSourcedId", "orgs"},
-			},
-		}
-		api.invoke()
-	}
+type nestedUid struct {
+	Type       string `json:"type" bson:"type,omitempty"`
+	Identifier string `json:"identifier" bson:"identifier,omitempty"`
+}
+
+type users struct {
+	SourcedId        string       `json:"sourcedId" bson:"sourcedId,omitempty"`
+	Status           string       `json:"status" bson:"status,omitempty"`
+	DateLastModified time.Time    `json:"dateLastModified" bson:"dateLastModified,omitempty"`
+	Username         string       `json:"username" bson:"username,omitempty"`
+	UserIds          []*nestedUid `json:"userIds" bson:"userIds,omitempty"`
+	EnabledUser      bool         `json:"enabledUser" bson:"enabledUser,omitempty"`
+	GivenName        string       `json:"givenName" bson:"givenName,omitempty"`
+	FamilyName       string       `json:"familyName" bson:"familyName,omitempty"`
+	MiddleName       string       `json:"middleName" bson:"middleName,omitempty"`
+	Role             string       `json:"role" bson:"role,omitempty"`
+	Identifier       string       `json:"identifier" bson:"identifier,omitempty"`
+	Email            string       `json:"email" bson:"email,omitempty"`
+	SMS              string       `json:"sms" bson:"sms,omitempty"`
+	Phone            string       `json:"phone" bson:"phone,omitempty"`
+	Agents           []*Nested    `json:"agents" bson:"agents,omitempty"`
+	Orgs             []*Nested    `json:"orgs" bson:"orgs,omitempty"`
+	Grades           []string     `json:"grades" bson:"grades,omitempty"`
+	Password         string       `json:"password" bson:"password,omitempty"`
 }
 
 var userCols = []string{
 	"sourcedId",
 	"status",
 	"dateLastModified",
-	"enabledUser",
-	"orgSourcedIds",
-	"role",
 	"username",
 	"userIds",
+	"enabledUser",
 	"givenName",
 	"familyName",
 	"middleName",
+	"role",
 	"identifier",
 	"email",
 	"sms",
 	"phone",
-	"agentSourcedIds",
+	"agents",
+	"orgs",
 	"grades",
 	"password",
+}
+
+func GetAllUsers(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("users")
+		GetCollection(c, asCols, w, r)
+	}
+}
+
+func GetUser(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("users")
+		GetDoc(c, asCols, w, r)
+	}
+}
+
+func PutUser(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("users")
+		var data users
+		data.DateLastModified = time.Now()
+		PutDoc(c, &data, w, r)
+	}
 }
