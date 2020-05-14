b634bab82d575f2049f7e84fc03896f3100b1b03
fffnite <bradley.whiteford@live.co.uk>
2020-05-14 01:04:33 +0100

    Add bulk user import from json array function

--- internal/handlers/users.go
+++ internal/handlers/users.go
@@ -1,8 +1,10 @@
 package handlers
 
 import (
+	"context"
 	"github.com/fffnite/go-oneroster/ormodel"
 	"github.com/go-chi/render"
+	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"net/http"
@@ -68,5 +70,37 @@ func PutUserId(client *mongo.Client) http.HandlerFunc {
 		c := client.Database("oneroster").Collection("users")
 		var data ormodel.NestedUid
 		PutNestedDoc(c, &data, "userIds", "type", w, r)
+	}
+}
+
+func PutBulkUser(client *mongo.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c := client.Database("oneroster").Collection("users")
+		var data []ormodel.Userwrap
+		// PutBulk(c, &data, w, r)
+
+		err := render.DecodeJSON(r.Body, &data)
+		if err != nil {
+			log.Error(err)
+			render.JSON(w, r, err)
+			return
+		}
+
+		var queries []mongo.WriteModel
+		for _, v := range data {
+			q := mongo.NewUpdateOneModel()
+			q.SetUpsert(true)
+			q.SetFilter(bson.D{{"sourcedId", v.User.SourcedId}})
+			q.SetUpdate(bson.D{{"$set", v.User}})
+			queries = append(queries, q)
+		}
+
+		ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+		defer cancel()
+		res, err := c.BulkWrite(ctx, queries)
+		if err != nil {
+			log.Error(err)
+		}
+		render.JSON(w, r, res)
 	}
 }
